3c5a883c6b37e59208cf5288cfea74274a2e30b3
Fotis Paschos <fpaschos@gmail.com>
2017-09-27 12:00:18 +0300

    Minor

--- src/main/scala/repository/interpreter/InMemoryTicketRepository.scala
+++ src/main/scala/repository/interpreter/InMemoryTicketRepository.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.{Map => MMap}
 import cats.syntax.either._
 
 case class InMemoryTicketRepository() extends TicketRepository {
-  lazy val internal = MMap.empty[String, Ticket]
+  lazy val internal: MMap[String, Ticket] = MMap.empty[String, Ticket]
 
   override def query(no: String): Either[String, Option[Ticket]] =
     internal.get(no).asRight[String]
